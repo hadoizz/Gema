954156d1e9451192da003974e278892d02437d64
filbert29 <fkhouwira70@gmail.com>
2021-05-14 19:33:02 +0700

    gak jadi pusying admin jadi

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -22,15 +22,33 @@ class Admin extends CI_Controller{
         $crud = new grocery_CRUD();
         $crud->set_table('barang')
              ->columns('Nama_Barang','Harga','Deskripsi','Gambar','Kategori')
-             ->fields('Nama_Barang','Harga','Deskripsi','Gambar','Kategori');
+             ->fields('Nama_Barang','Harga','Deskripsi','Gambar','Kategori')
+             ->set_relation('Kategori', 'kategori', 'deskripsi')
+             ->set_field_upload('Gambar', 'assets/uploads/poster')
+             ->callback_edit_field('Deskripsi', array($this, 'edit_description'))
+             ->callback_add_field('Deskripsi', array($this, 'add_description'))
+             ->callback_column('Gambar', array($this, 'img_size'));
             //  ->set_field_upload()
 
         $output = $crud->render();
         $data['crud'] = get_object_vars($output);
 
         $data['style'] = $this->load->view('include/ui',NULL,TRUE);
-        $data['groceryCRUD'] = $this->load->view('include/grocerycrud', $data);
+        $data['groceryCRUD'] = $this->load->view('include/grocerycrud', $data, TRUE);
         $this->load->view('admin/item', $data);
+    }
+
+    function edit_description($value, $primary_key){
+        return "<textarea name='Deskripsi'> $value </textarea>";
+    }
+
+    function add_description() {
+        return "<textarea name='Deskripsi'> </textarea> ";
+    }
+
+    function img_size($value){
+        $tes = base_url('/assets/uploads/poster/');
+        return "<img src='$tes$value' width='100px'> </img>";
     }
 }
 ?>
