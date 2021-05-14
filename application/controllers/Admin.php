5107b7e7948f668a1fe593d584dd3806b5a95b36
Bonaventura Sanjaya <bonaventura.sanjaya@student.umn.ac.id>
2021-05-14 19:09:56 +0700

    tes pusying

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,6 +5,7 @@ class Admin extends CI_Controller{
     public function __construct(){
 		parent::__construct();
         session_start();
+        $this->load->library('grocery_CRUD');
 	}
     
     public function index(){
@@ -18,7 +19,18 @@ class Admin extends CI_Controller{
     }
 
     public function crudItem(){
-        $this->load->view('admin/item');
+        $crud = new grocery_CRUD();
+        $crud->set_table('barang')
+             ->columns('Nama_Barang','Harga','Deskripsi','Gambar','Kategori')
+             ->fields('Nama_Barang','Harga','Deskripsi','Gambar','Kategori');
+            //  ->set_field_upload()
+
+        $output = $crud->render();
+        $data['crud'] = get_object_vars($output);
+
+        $data['style'] = $this->load->view('include/ui',NULL,TRUE);
+        $data['groceryCRUD'] = $this->load->view('include/grocerycrud', $data);
+        $this->load->view('admin/item', $data);
     }
 }
 ?>
--- application/views/admin/item.php
+++ application/views/admin/item.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Games</title>
+    <?= $style ?>
+    <?php echo $groceryCRUD ?>
+
 </head>
 <body>
     <h1>CRUD Games</h1>
+    <?php echo $crud['output']; ?>
 </body>
 </html>
