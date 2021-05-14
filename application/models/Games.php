7e5a2f950e923f70fc2c4a8112a35c9f8412270e
Bonaventura Sanjaya <bonaventura.sanjaya@student.umn.ac.id>
2021-05-14 20:41:49 +0700

    admin anjay mabar 123

--- application/models/Games.php
+++ application/models/Games.php
@@ -12,5 +12,18 @@ class Games extends CI_Model{
         $query = $this->db->query("SELECT * FROM barang");
         return $query->result_array(); 
     }
+
+    public function getLastGame(){
+        $last_row = $this->db->order_by('Id',"desc")
+            ->limit(1)
+            ->get('barang')
+            ->row()
+            ->Id;
+        return $last_row;
+        // $this->db->query("SELECT * FROM barang")->order_by('barang', 'DESC');
+        // $query = $this->db->get('barang')->order_by('Id','desc')->limit(1);
+        // var_dump($query);
+        // return  $query->result_array();
+    }
 }
 ?>
