19cdb0b7ab7fc1c47637f495740cde7cee295dee
Bonaventura Sanjaya <bonaventura.sanjaya@student.umn.ac.id>
2021-05-17 15:20:18 +0700

    apus func. games, tambah footer

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -29,22 +29,10 @@ class Base extends CI_Controller{
 
         $cart['cart'] = $this->load->view('components/cartModal',$rawCart, TRUE);
         $data['nav'] = $this->load->view('components/nav',$cart, TRUE);
+        $data['footer'] = $this->load->view('components/footer',NULL, TRUE);
         $data['style'] = $this->load->view('include/ui',NULL,TRUE);
         $data['showGames'] = $this->load->view('components/showGames',$games, TRUE);
         $this->load->view('pages/home',$data);
-    }
-    
-    public function games(){
-        $games['games'] = $this->games->getAllGames();
-        if(isset($_SESSION['email'])) $rawCart['items'] = $this->transaction->getCartValue($_SESSION['email']);
-        else $rawCart['items'] = array();
-
-        $cart['cart'] = $this->load->view('components/cartModal',$rawCart, TRUE);
-        $data['nav'] = $this->load->view('components/nav',$cart, TRUE);
-        $data['footer'] = $this->load->view('components/footer',NULL, TRUE);
-        $data['style'] = $this->load->view('include/ui', NULL, TRUE);
-        $data['showGames'] = $this->load->view('components/showGames',$games, TRUE);
-        $this->load->view('pages/games',$data);
     }
 
     public function details($id){
