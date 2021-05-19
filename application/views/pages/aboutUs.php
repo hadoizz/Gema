8d488a7fe668f4281b9b9810d94714f4fbf88893
Bonaventura Sanjaya <bonaventura.sanjaya@student.umn.ac.id>
2021-05-19 18:22:19 +0700

    aboutus responsive fix(2)

--- application/views/pages/aboutUs.php
+++ application/views/pages/aboutUs.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,137 +8,131 @@
     <title>Document</title>
     <?php echo $style ?>
 </head>
+
 <body>
     <?php echo $nav ?>
 
-<div class="ui four column grid container" id="main">
-    <div class="sixteen wide column">
-        <div class="ui horizontal inverted divider">Our GEMA Team</div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="ui card">
+    <div class="ui grid container" id="main">
+        <div class="sixteen wide column">
+            <div class="ui horizontal inverted divider">Our GEMA Team</div>
+        </div>
+        <div class="ui four stackable cards" style="margin-bottom: 50px">
+            <div class="card">
                 <div class="image">
                     <img src="<?= base_url('assets/img/coba.jpg') ?>">
                 </div>
                 <div class="content">
                     <a class="header">Najim Rizky</a>
                     <div class="meta">
-                    <span class="date">Joined in 2021</span>
+                        <span class="date">Joined in 2021</span>
                     </div>
                     <div class="description">
-                    Programmer 1
+                        Programmer 1
                     </div>
                     <div class="description">
-                    00000040113
+                        00000040113
                     </div>
                 </div>
                 <div class="extra content">
                     <a target="_blank" href="https://github.com/najimRizky">
-                    <i class="git icon"></i>
+                        <i class="git icon"></i>
                     </a>
                     <a target="_blank" href="https://www.instagram.com/nazky_r/">
-                    <i class="instagram icon"></i>
+                        <i class="instagram icon"></i>
                     </a>
                     <a target="_blank" href="https://www.linkedin.com/in/najim-rizky/">
-                    <i class="linkedin icon"></i>
+                        <i class="linkedin icon"></i>
                     </a>
                 </div>
             </div>
-        </div>
 
-        <div class="column">
-            <div class="ui card">
+            <div class="card">
                 <div class="image">
                     <img src="<?= base_url('assets/img/bona.jpg') ?>">
                 </div>
                 <div class="content">
                     <a class="header">Bonaventura Sanjaya</a>
                     <div class="meta">
-                    <span class="date">Joined in 2021</span>
+                        <span class="date">Joined in 2021</span>
                     </div>
                     <div class="description">
-                    Programmer 2
+                        Programmer 2
                     </div>
                     <div class="description">
-                    00000038083
+                        00000038083
                     </div>
                 </div>
                 <div class="extra content">
                     <a target="_blank" href="https://github.com/ayaayawae">
-                    <i class="git icon"></i>
+                        <i class="git icon"></i>
                     </a>
                     <a target="_blank" href="https://www.instagram.com/bonbonzay/">
-                    <i class="instagram icon"></i>
+                        <i class="instagram icon"></i>
                     </a>
                 </div>
             </div>
-        </div>
 
-        <div class="column">
-            <div class="ui card">
+            <div class="card">
                 <div class="image">
                     <img src="<?= base_url('assets/img/filbert.jpg') ?>">
                 </div>
                 <div class="content">
                     <a class="header">Filbert Khouwira</a>
                     <div class="meta">
-                    <span class="date">Joined in 2021</span>
+                        <span class="date">Joined in 2021</span>
                     </div>
                     <div class="description">
-                    Programmer 3
+                        Programmer 3
                     </div>
                     <div class="description">
-                    00000039724
+                        00000039724
                     </div>
                 </div>
                 <div class="extra content">
                     <a target="_blank" href="https://github.com/filbert29">
-                    <i class="git icon"></i>
+                        <i class="git icon"></i>
                     </a>
                     <a target="_blank" href="https://www.instagram.com/filbertkhouwira/">
-                    <i class="instagram icon"></i>
+                        <i class="instagram icon"></i>
                     </a>
                     <a target="_blank" href="https://www.facebook.com/filbert.khouwira.5">
-                    <i class="facebook icon"></i>
+                        <i class="facebook icon"></i>
                     </a>
                 </div>
             </div>
-        </div>
 
-        <div class="column">
-            <div class="ui card" style="margin-bottom: 30px;">
+            <div class="card">
                 <div class="image">
                     <img src="<?= base_url('assets/img/farhan.png') ?>">
                 </div>
                 <div class="content">
                     <a class="header">Mohamad Farhan</a>
                     <div class="meta">
-                    <span class="date">Joined in 2021</span>
+                        <span class="date">Joined in 2021</span>
                     </div>
                     <div class="description">
-                    Programmer 4
+                        Programmer 4
                     </div>
                     <div class="description">
-                    00000037928
+                        00000037928
                     </div>
                 </div>
                 <div class="extra content">
                     <a target="_blank" href="https://github.com/kejauhan">
-                    <i class="git icon"></i>
+                        <i class="git icon"></i>
                     </a>
                     <a target="_blank" href="https://www.instagram.com/farhannmf__/">
-                    <i class="instagram icon"></i>
+                        <i class="instagram icon"></i>
                     </a>
                     <a target="_blank" href="https://www.linkedin.com/in/mohamad-farhan-7469bb1b3/">
-                    <i class="linkedin icon"></i>
+                        <i class="linkedin icon"></i>
                     </a>
                 </div>
             </div>
         </div>
-     </div>
-</div>
+    </div>
 
     <?php echo $footer ?>
 </body>
+
 </html>
